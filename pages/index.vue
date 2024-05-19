b9051a1ece59121460565f19087d9709fde47d9b
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-19 19:31:47 -0300

    feat: update

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>Home Page</h1>
+    <!--Listagem dos contatos-->
+    <div class="w-full px-2">
+        <!--Item Contato-->
+        <div class="w-full p-4 border bg-gray-200 flex items-center justify-between">
+            
+        </div>
+    </div>
 </template>
