5b0b19592156116287b69adeff289cce66a9c9a5
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-26 12:32:16 -0300

    add: component ListItem

---
+++ components/ListItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full p-4 border bg-gray-200 flex items-center justify-between">
+            <span class="text-xl font-semibold">
+                <slot />
+            </span>
+            <div class="w-32 space-x-2">
+                <slot name="actions" />
+            </div>
+        </div>
+</template>
