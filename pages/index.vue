f85e7e96a698ccefb10e8b8bbddde23da5900541
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-05-26 13:39:30 -0300

    feat: update

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,7 @@
 <template>
     <!--Listagem dos contatos-->
     <div class="w-full px-2">
-        <!--Item Contato
-        <div class="w-full p-4 border bg-gray-200 flex items-center justify-between">
-            <span class="text-xl font-semibold">João da Silva</span>
-
-            <div class="w-32 space-x-2">
-                <button>Editar</button>
-                <button>Excluir</button>
-            </div>
-        </div> -->
+        <!--Item Contato-->
         <ListItem>
             João da Silva
 
@@ -20,10 +12,6 @@
         </ListItem>
     </div>
 
-<!--
-    <button class="w-full p-4 mt-8 border b-blue-200 rounded bg-blue-500 text-white flex items-center justify-center">
-        Adicionar Contato
-    </button>
--->
+    <!--Botão Adicionar-->
     <UIButton>Adicionar Contato</UIButton>
 </template>
